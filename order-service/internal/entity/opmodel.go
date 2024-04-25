765fd679b17714cbd66ba89dd9ab88b1b7e8fa37
keldimurodov <keldimurodovsardor@gmail.com>
2024-04-25 19:04:33 +0500

    Added orders_products table

---
+++ order-service/internal/entity/opmodel.go
@@ -0,0 +1,7 @@
+package entity
+
+type OPModel struct {
+	Id string
+	OrderId int64
+	ProductID int64
+}
---
+++ order-service/migrations/000002_products.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(65),
+    description VARCHAR(255),
+    price INT,
+    discount INT,
+    picture TEXT,
+    category_id INT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP DEFAULT NULL
+);
