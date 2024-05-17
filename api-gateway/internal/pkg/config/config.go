f3988d34f422806daf23635b58f94044ab6b1101
rarebek <nomonovn2@gmail.com>
2024-05-17 20:19:03 +0500

    changed config files...

--- api-gateway/internal/pkg/config/config.go
+++ api-gateway/internal/pkg/config/config.go
@@ -117,7 +117,6 @@
 // 	return defaultValue
 // }
 
-
 package config
 
 import (
@@ -189,7 +188,7 @@ func NewConfig() Config {
 	config.Context.Timeout = getEnv("CONTEXT_TIMEOUT", "30s")
 
 	// server configuration
-	config.Server.Host = getEnv("SERVER_HOST", "localhost")
+	config.Server.Host = getEnv("SERVER_HOST", "")
 	config.Server.Port = getEnv("SERVER_PORT", ":8080")
 	config.Server.ReadTimeout = getEnv("SERVER_READ_TIMEOUT", "10s")
 	config.Server.WriteTimeout = getEnv("SERVER_WRITE_TIMEOUT", "10s")
@@ -199,21 +198,21 @@ func NewConfig() Config {
 	config.AccessTokenTimout = cast.ToInt(getEnv("ACCESS_TOKEN_TIMOUT", "30000"))
 
 	// redis configuration
-	config.Redis.Host = getEnv("REDIS_HOST", "localhost")
+	config.Redis.Host = getEnv("REDIS_HOST", "redis")
 	config.Redis.Port = getEnv("REDIS_PORT", "6379")
 	config.Redis.Password = getEnv("REDIS_PASSWORD", "")
 	config.Redis.Name = getEnv("REDIS_DATABASE", "0")
 
 	// USER
-	config.UserService.Host = getEnv("USER_SERVICE_GRPC_HOST", "127.0.0.1")
+	config.UserService.Host = getEnv("USER_SERVICE_GRPC_HOST", "user-service")
 	config.UserService.Port = getEnv("USER_SERVICE_GRPC_PORT", ":2222")
 
 	// PRODUCT
-	config.ProductService.Host = getEnv("PRODUCT_SERVICE_GRPC_HOST", "127.0.0.1")
+	config.ProductService.Host = getEnv("PRODUCT_SERVICE_GRPC_HOST", "product-service")
 	config.ProductService.Port = getEnv("PRODUCT_SERVICE_GRPC_PORT", ":1111")
 
 	// ORDER
-	config.OrderService.Host = getEnv("ORDER_SERVICE_GRPC_HOST", "127.0.0.1")
+	config.OrderService.Host = getEnv("ORDER_SERVICE_GRPC_HOST", "order-service")
 	config.OrderService.Port = getEnv("ORDER_SERVICE_GRPC_PORT", ":3333")
 
 	// casbin
